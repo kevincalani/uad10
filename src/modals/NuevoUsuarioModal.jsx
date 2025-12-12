e076fd853d7cfa9946d8e12f1fffadc707be6463
Kevin Calani <kevincalani2311@gmail.com>
2025-12-12 01:14:33 -0400

    Fix: normalizar ruta useModal.jsx y resolver conflicto de casing

--- src/modals/NuevoUsuarioModal.jsx
+++ src/modals/NuevoUsuarioModal.jsx
@@ -57,7 +57,7 @@ export default function NuevoUsuarioModal({ onClose, onSuccess }) {
   };
 
   return (
-    <div className="max-w-full">
+    <div className=" bg-white max-w-full">
       {/* HEADER */}
       <div className="flex justify-between bg-blue-700 rounded-t-lg items-center border-b border-gray-50 p-3 mb-3">
         <div className="flex items-center gap-2 text-white font-bold text-base">
